35ff009e55d5b4a357d16cd4af044a7c302e23db
complicat9d <ivanov.stuff@mail.ru>
2024-11-18 18:38:50 +0300

    add caching to retrieving responses from openai api calls

--- utils/report.py
+++ utils/report.py
@@ -1,3 +1,5 @@
+import hashlib
+import cachetools
 import openai
 import backoff
 from typing import List
@@ -8,9 +10,17 @@ from config import settings
 
 openai.api_key = settings.API_KEY
 
+report_cache = cachetools.TTLCache(maxsize=100, ttl=3600)
+
 
 @backoff.on_exception(backoff.expo, openai.RateLimitError)
 def generate_report(products: List[ProductSchema]) -> str:
+    product_hash = hashlib.sha256(str(products).encode("utf-8")).hexdigest()
+    logger.debug("Hash for the given products: {}".format(product_hash))
+    if product_hash in report_cache:
+        logger.info("Using cached OpenAI API response")
+        return report_cache[product_hash]
+
     total_revenue = sum(product.quantity * product.price for product in products)
     top_products = sorted(products, key=lambda x: x.quantity, reverse=True)[:3]
     categories = {
@@ -34,10 +44,18 @@ def generate_report(products: List[ProductSchema]) -> str:
             messages=[{"role": "user", "content": prompt}],
         )
 
-        return response.choices[0].message.content
+        report_content = response.choices[0].message.content
+        report_cache[product_hash] = report_content
+
+        return report_content
+
     except openai.RateLimitError:
-        logger.warning("The limit of tokens for the given API key has been exceeded")
+        logger.warning(
+            "The limit of tokens for the given API key has been exceeded, report has not been added"
+        )
     except openai.NotFoundError:
         logger.warning(
             "The given model name does not exist or you do not have access to it"
         )
+    except Exception as e:
+        logger.error(f"Error while generating report: {e}")
